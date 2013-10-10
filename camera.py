e670481d4f2461a9df009bd3978ac5b294f52004
Clemens N. Klokmose <clemens@cs.au.dk>
2013-10-10 14:33:09 +0000

    Changed port and ip

--- camera.py
+++ camera.py
@@ -22,4 +22,4 @@ def snapshot():
     return send_file(imageBuffer, mimetype='image/jpeg')
     
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0', port=80)
